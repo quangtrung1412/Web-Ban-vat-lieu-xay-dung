b478131a79a9193f412a56c71011f86ecd0b7052
trunglq2 <trunglq2@fpts.com.vn>
2023-04-24 16:51:02 +0700

    add lib boostrap

--- Interface/wwwroot/js/layout.js
+++ Interface/wwwroot/js/layout.js
@@ -2,3 +2,22 @@ $("#navbarSupportedContent li").click(function () {
     $("#navbarSupportedContent li").removeClass('active');
     $(this).addClass('active');
 });
+$('#navbarLogin .dropdown .avatar').on('click', function () {
+    $(".user-dropdown").show();
+});
+
+var allOfThem = $('*');
+for (let i = 0; i < allOfThem.length; i++) {
+    // text += allOfThem[i] + "<br>";
+    if (allOfThem[i].className.length != 0) {
+        let text = "." + allOfThem[i].className;
+        console.log(text);
+    }
+    if (allOfThem[i].className != 'user-dropdown' || allOfThem[i].className != 'nav-link avatar' || allOfThem[i].className.length != 0) {
+        let text = "." + allOfThem[i].className;
+        console.log(text);
+        // $(text).on('click', function () {
+        //     $(".user-dropdown").hide();
+        // });
+    }
+}    
